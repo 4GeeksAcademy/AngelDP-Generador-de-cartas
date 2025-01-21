deea2c30e1544be2bd29510cc93dcc3611a2a74f
Angel Ponte <angeld0606@gmail.com>
2025-01-21 16:57:00 +0000

    ejercicio resuelto

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let myButton = document.getElementById("myButton")
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let type = ["♥️", "♠︎", "♦︎", "♣︎"];
+let cartas = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+let colors = ["red", "black"]
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function randomIndex (arr){
+  let newIndex = Math.floor(Math.random() * arr.length);
+  return arr[newIndex];
+}
+
+function GeneraUnaCarta () {
+  
+  let newType = randomIndex(type);
+  let newNumber = randomIndex(cartas);
+  let newColor = randomIndex(colors);
+
+
+  document.querySelector("#topSymbol").innerHTML = newType;
+  document.querySelector("#topSymbol").style.color = newColor;
+
+  document.querySelector("#centerNumber").innerHTML = newNumber;
+  document.querySelector("#centerNumber").style.color = newColor;
+
+  document.querySelector("#bottomSymbol").innerHTML = newType;
+  document.querySelector("#bottomSymbol").style.color = newColor;
 };
 
+window.onload = function() {
+
+  GeneraUnaCarta ()
+
+
+  document.querySelector("#generarCarta").addEventListener("click", GeneraUnaCarta);
+}
